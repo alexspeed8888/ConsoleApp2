aeea93b615a0796703b6d29aa42bcc57fbdef126
ALEX <ALEX@DESKTOP-PGQ8CUS>
2021-06-17 12:29:37 +0200

    Agregar archivos de proyecto.

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i1 = 5;
+            int i2 = 6;
+            double d1 = 2.57;
+            double suma = i1 + d1;
+            Console.WriteLine("Hello World!");
+            string valor = Console.ReadLine();
+            Console.WriteLine("El número introducido es: "+valor);
+            int valorInt = Convert.ToInt32(valor);
+            Console.ReadKey();
+        }
+    }
+}
